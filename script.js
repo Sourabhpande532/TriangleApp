eae6d477c87e5646267cdd86358fc32eeedc4c6d
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-11-28 15:39:35 +0530

    include script and check

---
+++ script.js
@@ -0,0 +1,4 @@
+const angleInputs = document.querySelectorAll(".user-angle-input");
+const isAngleButton = document.querySelector("#is-button-triangle");
+const showResult = document.querySelector("#display-output");
+console.log(angleInputs[0]);
