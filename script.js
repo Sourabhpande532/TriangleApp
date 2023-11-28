2a924034bf69ab39ab6be0db6da18033f78e74cd
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-11-28 20:08:23 +0530

    refactor & Build logic : Input-output-process[C1]

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const angleInputs = document.querySelectorAll(".user-angle-input");
-const isAngleButton = document.querySelector("#is-button-triangle");
-const showResult = document.querySelector("#display-output");
-console.log(angleInputs[0]);
+const angleInputs = document.querySelectorAll( ".user-angle-input" );
+const isTriangleButton = document.querySelector( "#is-button-triangle" );
+const showResultEl = document.querySelector( ".display-output" );
+
+function calculateSumOfAngles( A1, A2, A3 ) {
+    const sumOfAngles = A1 + A2 + A3;
+    return sumOfAngles;
+}
+
+function isTriangle() {
+    const sumOfAngles = calculateSumOfAngles(
+        Number( angleInputs[0].value ),
+        Number( angleInputs[1].value ),
+        Number( angleInputs[2].value )
+    );
+    if(sumOfAngles===180){
+        showResultEl.innerText = "Yehh!😍 The Angle is forming Triangle"
+    }else{
+        showResultEl.innerText = "Oohh!😔 Sadly The Angle not Formed Triangle"
+    }
+}
+
+isTriangleButton.addEventListener( "click", isTriangle );
