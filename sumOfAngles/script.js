9dfd9752ed0294fe7aeaecc41d96105abd738fcd
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-11-30 16:11:40 +0530

    refactor - related style and error validation

--- sumOfAngles/script.js
+++ sumOfAngles/script.js
@@ -1,23 +1,26 @@
-const angleInputs = document.querySelectorAll( ".user-angle-input" );
-const isTriangleButton = document.querySelector( "#is-button-triangle" );
-const showResultEl = document.querySelector( ".display-output" );
+const angleInputs = document.querySelectorAll(".user-angle-input");
+const isTriangleButton = document.querySelector("#is-button-triangle");
+const showResultEl = document.querySelector(".display-output");
 
-function calculateSumOfAngles( A1, A2, A3 ) {
-    const sumOfAngles = A1 + A2 + A3;
-    return sumOfAngles;
+function calculateSumOfAngles(A1, A2, A3) {
+  const sumOfAngles = A1 + A2 + A3;
+  return sumOfAngles;
 }
-
 function isTriangle() {
-    const sumOfAngles = calculateSumOfAngles(
-        Number( angleInputs[0].value ),
-        Number( angleInputs[1].value ),
-        Number( angleInputs[2].value )
-    );
-    if(sumOfAngles===180){
-        showResultEl.innerText = "Yehh!😍 The Angle is forming Triangle"
-    }else{
-        showResultEl.innerText = "Oohh!😔 Sadly The Angle not Formed Triangle"
+  let angleA = Number(angleInputs[0].value);
+  let angleB = Number(angleInputs[1].value);
+  let angleC = Number(angleInputs[2].value);
+
+  if (angleA && angleB && angleC) {
+    const sumOfAngles = calculateSumOfAngles(angleA, angleB, angleC);
+    if (sumOfAngles === 180) {
+      showResultEl.innerText = "Yehh!😍 The Angle is forming Triangle";
+    } else {
+      showResultEl.innerText = "Oohh!😔 Sadly The Angle not Formed Triangle";
     }
+  } else {
+    showResultEl.innerText = "Oohh!🤔 all field is required";
+  }
 }
 
-isTriangleButton.addEventListener( "click", isTriangle );
+isTriangleButton.addEventListener("click", isTriangle);
