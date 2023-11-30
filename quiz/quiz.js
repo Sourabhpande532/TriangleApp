b066a79cb59c29e133b9a883bca59d5be0af1aaa
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-11-30 15:46:39 +0530

    refactor - related to style & validation

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,12 +1,34 @@
 const quizForm = document.querySelector( ".quiz-form" );
 const submitAnswerButton = document.querySelector( "#submit-answers" );
 const outputResultEl = document.querySelector( "#show-result" );
+const timerEl = document.querySelector( "#timer" );
 
 const correctAnswer = ["90°", "right angled"];
+let timer;
+let remainingSeconds = 60;
 
+/* Timer */
+function timeManager() {
+  timerEl.innerText = `Remaining time left is ${ remainingSeconds } sec`;
+  if ( remainingSeconds === 0 ) {
+    clearInterval( timer );
+    calculateScore();
+  } else {
+    remainingSeconds--;
+  }
+}
+/* calculateScore */
 function calculateScore() {
+  clearInterval( timer );
   let score = 0;
   let index = 0;
+  if (
+    !quizForm.elements.question1.value ||
+    !quizForm.elements.question2.value
+  ) {
+    outputResultEl.innerText = "Please answer all questions.";
+    return;
+  }
   const quizFormResults = new FormData( quizForm );
   for ( let value of quizFormResults.values() ) {
     if ( value === correctAnswer[index] ) {
@@ -14,7 +36,10 @@ function calculateScore() {
     }
     index += 1;
   }
-  outputResultEl.innerText = "Your score is " + score
+  outputResultEl.innerText = `Your score is ${ score }. Time taken: ${ 60 - remainingSeconds
+    } seconds.`;
 }
 
 submitAnswerButton.addEventListener( "click", calculateScore );
+
+timer = setInterval( timeManager, 1000 );
